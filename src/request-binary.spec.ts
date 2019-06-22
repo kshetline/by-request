b370ab4ded1fb7e2501cb243b3f257de7166e391
Kerry Shetline <kerry@shetline.com>
2019-06-22 13:32:24 -0400

    Add unit test for defeating automatic decompression.

--- src/request-binary.spec.ts
+++ src/request-binary.spec.ts
@@ -1,5 +1,6 @@
 import { requestBinary } from './request-binary';
 import { port } from './test-server.spec';
+import * as zlib from 'zlib';
 
 describe('request-binary', () => {
   it('should read binary data correctly', async done => {
@@ -8,6 +9,22 @@ describe('request-binary', () => {
 
     content = await requestBinary('https://opensource.org/licenses/MIT');
     expect(content.toString('utf8')).toContain('copies or substantial portions');
+
+    done();
+  });
+
+  it('should be able to defeat automatic decompression of a gzipped response', async done => {
+    const content = await requestBinary(`http://localhost:${port}/test8/`, { dontDecompress: true });
+    let unzipped = Buffer.alloc(0);
+
+    try {
+      unzipped = zlib.gunzipSync(content);
+    }
+    catch (err) {
+      expect(false).toBeTruthy(err);
+    }
+
+    expect(unzipped.toString()).toContain('Very large content');
 
     done();
   });
